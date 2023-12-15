bcbffeab8f9a155687e528e2d30bdbe486300d6b
Carlos Chacon <carlosdchaconmendez@gmail.com>
2023-12-15 11:41:26 -0400

    react native app initialized

---
+++ src/screens/HelloWorld.tsx
@@ -0,0 +1,14 @@
+import {ScrollView, Text, View} from 'react-native';
+import React from 'react';
+
+function HelloWorld(): React.JSX.Element {
+  return (
+    <ScrollView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default HelloWorld;
